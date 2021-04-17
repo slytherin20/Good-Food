3c01bf152a38d1f29c6a337eb33a2672a0934d3a
Sonali Gupta <36319762+slytherin20@users.noreply.github.com>
2021-04-17 10:08:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <nav className="navbar">Good Books</nav>
+      <div clasName="container">
+        <div className="show-genre"></div>
+        <div className="show-books"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,84 @@
+const books = {
+  romance: [
+    {
+      name: "Pride and Prejudice",
+      author: "Jane Austin",
+      publishyear: 1893,
+      rating: "4.5/5"
+    },
+    {
+      name: "Jane Eyre",
+      author: "Charlotte Bronte",
+      publishyear: 1847,
+      rating: "4/5"
+    },
+    {
+      name: "The fault in our stars",
+      author: "John Green",
+      publishyear: 2013,
+      rating: "4.7/5"
+    }
+  ],
+  thriller: [
+    {
+      name: "Gone girl",
+      author: "Gillian Flynn",
+      publishyear: 2014,
+      rating: "4/5"
+    },
+    {
+      name: "The girl with the dragon tattoo",
+      author: "Steig Larson",
+      publishyear: 2008,
+      rating: "4.1/5"
+    },
+    {
+      name: "And then there were none",
+      author: "Agatha Christie",
+      publishyear: 2004,
+      rating: "4.2/5"
+    }
+  ],
+  horror: [
+    {
+      name: "The shining",
+      author: "Stephen King",
+      publishyear: 1977,
+      rating: "4.2/5"
+    },
+    {
+      name: "It",
+      author: "Stephen King",
+      publishyear: 1986,
+      rating: "4.2/5"
+    },
+    {
+      name: "Frankenstein",
+      author: "Mary Wollstonecraft Shelly",
+      publishyear: 1818,
+      rating: "3.9/5"
+    },
+    {
+      name: "The haunting of the hill house",
+      author: "Shirley Jackson",
+      publishyear: 1959,
+      rating: "3.8/5"
+    }
+  ],
+  javascript: [
+    {
+      name: "Eloquent Javascript,3rd Edition",
+      author: "Marijn Haverbekedives",
+      publishyear: 2018,
+      rating: "4.6/5"
+    },
+    {
+      name: "Javascript: The good parts",
+      author: "D Crockford",
+      publishyear: 2008,
+      rating: "4.4/5"
+    }
+  ]
+};
+
+export default books;
